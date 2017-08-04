00c0825e78f3bd578c6ad151902946418199587b
Christoforos <chrischaralambous14@gmail.com>
2017-08-04 23:38:36 +0300

    Changed quotechar

--- test.py
+++ test.py
@@ -2,14 +2,17 @@ import os
 import elcrypt
 import csv
 
-os.system("git clone --depth=50 --branch=master https://github.com/elcrypt/elcrypt-tests.git")
+#os.system("git clone --depth=50 --branch=master https://github.com/elcrypt/elcrypt-tests.git")
 
-with open('elcrypt-tests/tests.csv', 'r') as datafile:
-    datareader = csv.reader(datafile,delimiter=',',quotechar='"')
+with open('tests.csv', 'r') as datafile:
+    datareader = csv.reader(datafile,delimiter=',',quotechar=' ')
     data = []
     for row in datareader:
         data.append(row)
 
 for test in data:
     if (elcrypt.hash(test[0],test[1]) != test[2]):
+        print("Given: "+test[0])
+        print("Got: "+elcrypt.hash(test[0],test[1]))
+        print("Expected: "+test[2])
         raise Exception("Test "+test[0]+" failed")
