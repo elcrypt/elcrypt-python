b653d5b62787c081b16d7ad8906a8b64448d9979
Christoforos <chrischaralambous14@gmail.com>
2017-08-05 16:16:19 +0300

    Fixup

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-git clone --depth=50 --branch=master https://github.com/elcrypt/elcrypt-tests.git
+#git clone --depth=50 --branch=master https://github.com/elcrypt/elcrypt-tests.git
 
-current=0
+current=-1
 while read line; do
+	let "current+=1"
 	if [ $current == 0 ]; then
 		input=$(echo -n "$line")
 	elif [ $current == 1 ]; then
@@ -20,7 +21,6 @@ while read line; do
 			echo "Expected: $expected"
 			exit 1
 		fi
-		current=0
-	fi
-	current=$current+1
+		current=-1
+	fi	
 done < "elcrypt-tests/tests.csv"
