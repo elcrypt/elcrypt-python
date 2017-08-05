7729c3f546e81096aa553cc91dbb39dcc5d64252
Christoforos <chrischaralambous14@gmail.com>
2017-08-05 16:26:10 +0300

    Enable git downloading again

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#git clone --depth=50 --branch=master https://github.com/elcrypt/elcrypt-tests.git
+git clone --depth=50 --branch=master https://github.com/elcrypt/elcrypt-tests.git
 
 current=-1
 while read line; do
